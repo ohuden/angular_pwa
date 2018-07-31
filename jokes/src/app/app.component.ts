a5f6890d17f8d0fb7b9efd3a4ef327aae6e98239
Oleg Khudenko <ohuden@softserveinc.com>
2018-07-31 14:41:24 +0300

    added final func

--- jokes/src/app/app.component.ts
+++ jokes/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  update = false;
+  joke: any;
+  constructor(updates: SwUpdate, private data: DataService) {
+    updates.available.subscribe(event => {
+      // this.update = true;
+
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+  }
+
+  ngOnInit() {
+    this.data.gimmeJokes().subscribe(res => {
+      this.joke = res;
+    });
+  }
 }
--- jokes/src/app/app.module.ts
+++ jokes/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,7 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
